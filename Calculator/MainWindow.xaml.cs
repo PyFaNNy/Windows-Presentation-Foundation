a1c6bd9455efbfdbbf4c61006dd21cecc34d2373
Mr.Bubble <puzyrev.artyom@gmail.com>
2021-04-15 21:04:35 +0300

    Done Calculator

--- Calculator/MainWindow.xaml.cs
+++ Calculator/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -23,6 +24,33 @@ namespace Calculator
         public MainWindow()
         {
             InitializeComponent();
+
+            foreach (UIElement item in MainRoot.Children)
+            {
+                if (item is Button)
+                {
+                    ((Button)item).Click += Button_Click;
+                }
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string str = (string)((Button)e.OriginalSource).Content;
+
+            if (str == "AC")
+            {
+                OutputWindow.Text = string.Empty;
+            }
+            else if (str == "=")
+            {
+                string value = new DataTable().Compute(OutputWindow.Text,null).ToString();
+                OutputWindow.Text = value;
+            }
+            else
+            {
+                OutputWindow.Text += str;
+            }
         }
     }
 }
