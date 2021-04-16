c0b35cb0f4b9faf64760a85f22e38170c31a89d9
Mr.Bubble <puzyrev.artyom@gmail.com>
2021-04-16 16:09:54 +0300

    Змейка готова

--- Shake/MainWindow.xaml.cs
+++ Shake/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
@@ -18,6 +19,8 @@ namespace Shake
         const int SnakeStartLength = 3;
         const int SnakeStartSpeed = 400;
         const int SnakeSpeedThreshold = 100;
+        private int snakeLength;
+        private int currentScore = 0;
         private Random rnd = new Random();
         private DispatcherTimer gameTickTimer = new DispatcherTimer();
         private SolidColorBrush snakeBodyBrush = Brushes.Green;
@@ -28,7 +31,6 @@ namespace Shake
 
         public enum SnakeDirection { Left, Right, Up, Down };
         private SnakeDirection snakeDirection = SnakeDirection.Right;
-        private int snakeLength;
         public MainWindow()
         {
             InitializeComponent();
@@ -147,10 +149,10 @@ namespace Shake
                 Position = new Point(nextX, nextY),
                 IsHead = true
             });
-            //... and then have it drawn!  
+            //... and then have it drawn!
             DrawSnake();
-            // We'll get to this later...  
-            //DoCollisionCheck();          
+            // Finally: Check if it just hit something!
+            DoCollisionCheck();
         }
 
         public void SnakeWPFSample()
@@ -165,14 +167,29 @@ namespace Shake
 
         private void StartNewGame()
         {
+            // Remove potential dead snake parts and leftover food...
+            foreach (SnakePart snakeBodyPart in snakeParts)
+            {
+                if (snakeBodyPart.UiElement != null)
+                    GameArea.Children.Remove(snakeBodyPart.UiElement);
+            }
+            snakeParts.Clear();
+            if (snakeFood != null)
+                GameArea.Children.Remove(snakeFood);
+
+            // Reset stuff
+            currentScore = 0;
             snakeLength = SnakeStartLength;
             snakeDirection = SnakeDirection.Right;
             snakeParts.Add(new SnakePart() { Position = new Point(SnakeSquareSize * 5, SnakeSquareSize * 5) });
             gameTickTimer.Interval = TimeSpan.FromMilliseconds(SnakeStartSpeed);
             gameTickTimer.Tick += GameTickTimer_Tick;
-            // Draw the snake and the snake food
+            // Draw the snake again and some new food...
             DrawSnake();
             DrawSnakeFood();
+
+            // Update status
+            UpdateGameStatus();
 
             // Go!        
             gameTickTimer.IsEnabled = true;
@@ -221,6 +238,47 @@ namespace Shake
             }
             if (snakeDirection != originalSnakeDirection)
                 MoveSnake();
+        }
+        private void DoCollisionCheck()
+        {
+            SnakePart snakeHead = snakeParts[snakeParts.Count - 1];
+
+            if ((snakeHead.Position.X == Canvas.GetLeft(snakeFood)) && (snakeHead.Position.Y == Canvas.GetTop(snakeFood)))
+            {
+                EatSnakeFood();
+                return;
+            }
+
+            if ((snakeHead.Position.Y < 0) || (snakeHead.Position.Y >= GameArea.ActualHeight) ||
+            (snakeHead.Position.X < 0) || (snakeHead.Position.X >= GameArea.ActualWidth))
+            {
+                EndGame();
+            }
+
+            foreach (SnakePart snakeBodyPart in snakeParts.Take(snakeParts.Count - 1))
+            {
+                if ((snakeHead.Position.X == snakeBodyPart.Position.X) && (snakeHead.Position.Y == snakeBodyPart.Position.Y))
+                    EndGame();
+            }
+        }
+        private void EatSnakeFood()
+        {
+            snakeLength++;
+            currentScore++;
+            int timerInterval = Math.Max(SnakeSpeedThreshold, (int)gameTickTimer.Interval.TotalMilliseconds - (currentScore * 2));
+            gameTickTimer.Interval = TimeSpan.FromMilliseconds(timerInterval);
+            GameArea.Children.Remove(snakeFood);
+            DrawSnakeFood();
+            UpdateGameStatus();
+        }
+        private void UpdateGameStatus()
+        {
+            this.Title = "SnakeWPF - Score: " + currentScore + " - Game speed: " + gameTickTimer.Interval.TotalMilliseconds;
+        }
+        private void EndGame()
+        {
+            gameTickTimer.IsEnabled = false;
+            MessageBox.Show("Oooops, you died!\n\nTo start a new game, just press the Space bar...", "SnakeWPF");
         }
     }
 }
