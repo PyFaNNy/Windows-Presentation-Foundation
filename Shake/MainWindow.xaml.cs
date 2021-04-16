ac5473be75dbfc5bcb7a57427fe21d4df5daefb9
Mr.Bubble <puzyrev.artyom@gmail.com>
2021-04-16 14:51:13 +0300

    Добавил движение змейки

--- Shake/MainWindow.xaml.cs
+++ Shake/MainWindow.xaml.cs
@@ -1,18 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
-using System.Windows.Threading;
 
 namespace Shake
 {
@@ -22,6 +14,13 @@ namespace Shake
     public partial class MainWindow : Window
     {
         const int SnakeSquareSize = 20;
+        private SolidColorBrush snakeBodyBrush = Brushes.Green;
+        private SolidColorBrush snakeHeadBrush = Brushes.YellowGreen;
+        private List<SnakePart> snakeParts = new List<SnakePart>();
+
+        public enum SnakeDirection { Left, Right, Up, Down };
+        private SnakeDirection snakeDirection = SnakeDirection.Right;
+        private int snakeLength;
         public MainWindow()
         {
             InitializeComponent();
@@ -65,5 +64,74 @@ namespace Shake
                     doneDrawingBackground = true;
             }
         }
+
+        private void DrawSnake()
+        {
+            foreach (SnakePart snakePart in snakeParts)
+            {
+                if (snakePart.UiElement == null)
+                {
+                    snakePart.UiElement = new Rectangle()
+                    {
+                        Width = SnakeSquareSize,
+                        Height = SnakeSquareSize,
+                        Fill = (snakePart.IsHead ? snakeHeadBrush : snakeBodyBrush)
+                    };
+                    GameArea.Children.Add(snakePart.UiElement);
+                    Canvas.SetTop(snakePart.UiElement, snakePart.Position.Y);
+                    Canvas.SetLeft(snakePart.UiElement, snakePart.Position.X);
+                }
+            }
+        }
+
+        private void MoveSnake()
+        {
+            // Remove the last part of the snake, in preparation of the new part added below  
+            while (snakeParts.Count >= snakeLength)
+            {
+                GameArea.Children.Remove(snakeParts[0].UiElement);
+                snakeParts.RemoveAt(0);
+            }
+            // Next up, we'll add a new element to the snake, which will be the (new) head  
+            // Therefore, we mark all existing parts as non-head (body) elements and then  
+            // we make sure that they use the body brush  
+            foreach (SnakePart snakePart in snakeParts)
+            {
+                (snakePart.UiElement as Rectangle).Fill = snakeBodyBrush;
+                snakePart.IsHead = false;
+            }
+
+            // Determine in which direction to expand the snake, based on the current direction  
+            SnakePart snakeHead = snakeParts[snakeParts.Count - 1];
+            double nextX = snakeHead.Position.X;
+            double nextY = snakeHead.Position.Y;
+            switch (snakeDirection)
+            {
+                case SnakeDirection.Left:
+                    nextX -= SnakeSquareSize;
+                    break;
+                case SnakeDirection.Right:
+                    nextX += SnakeSquareSize;
+                    break;
+                case SnakeDirection.Up:
+                    nextY -= SnakeSquareSize;
+                    break;
+                case SnakeDirection.Down:
+                    nextY += SnakeSquareSize;
+                    break;
+            }
+
+            // Now add the new head part to our list of snake parts...  
+            snakeParts.Add(new SnakePart()
+            {
+                Position = new Point(nextX, nextY),
+                IsHead = true
+            });
+            //... and then have it drawn!  
+            DrawSnake();
+            // We'll get to this later...  
+            //DoCollisionCheck();          
+        }
+
     }
 }
---
+++ Shake/SnakePart.cs
@@ -0,0 +1,13 @@
+﻿using System.Windows;
+
+namespace Shake
+{
+    class SnakePart
+    {
+        public UIElement UiElement { get; set; }
+
+        public Point Position { get; set; }
+
+        public bool IsHead { get; set; }
+    }
+}
